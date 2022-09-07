fb52dc827d05323a84879cacd5878c26c4b7b1f8
WarunaSandesh2001 <warunasandesh2001@gmail.com>
2022-09-07 22:24:20 +0530

    added Customer Page

--- src/app/App.js
+++ src/app/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
-
+import Customer from "../pages/Customer";
 
 
 function App() {
   return (
       <Routes>
           <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/customer' element={<Customer/>}/>
 
           <Route path="*" element={<NotFound/>}/>
       </Routes>
